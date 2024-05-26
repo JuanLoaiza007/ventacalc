df6c6889a1b3d4ff883b7aa123dfdda236307582
Juan David <juan.loaiza.santiago@correounivalle.edu.co>
2024-05-26 17:39:26 -0500

    style changes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,12 +1,13 @@
 // [Footer.jsx]
 import React from 'react'
 import { AppState } from '../states/AppState'
+import './Footer.css'
 
 export default function Footer () {
   const useAppState = AppState()
   return (
-    <footer className='bg-dark text-white'>
-      <div className='text-center p-2'>v{useAppState.appVersion}</div>
+    <footer className='bg-dark text-white footer-container'>
+      <div className='py-2'>v{useAppState.appVersion}</div>
     </footer>
   )
 }
--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,13 +1,16 @@
 // [Navigation.jsx]
 import React from 'react'
 import { AppState } from '../states/AppState'
+import './Navigation.css'
 
 export default function Navigation ({ logoApp }) {
   const useAppState = AppState()
   return (
-    <nav className='navbar navbar-dark bg-dark px-2'>
-      <img src={logoApp} width='30' height='30' className='mx-2' alt='logo' />
-      <div className='navbar-brand h4 text-white me-auto'>{useAppState.appName}</div>
+    <nav className='navbar navbar-dark bg-dark px-2 '>
+      <div className='d-flex flex-row navigation-container'>
+        <img src={logoApp} className='logo-icon mx-2' alt='logo' />
+        <div className='app-name'>{useAppState.appName}</div>
+      </div>
     </nav>
   )
 }
