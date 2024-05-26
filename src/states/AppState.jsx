52e67dd62b7a2f5672a98440461c60305af62dd8
Juan David <juan.loaiza.santiago@correounivalle.edu.co>
2024-05-26 18:53:16 -0500

    update semver

--- src/states/AppState.jsx
+++ src/states/AppState.jsx
@@ -3,7 +3,7 @@ import { create } from 'zustand'
 
 export const AppState = create((set) => ({
   appName: 'Ventacalc',
-  appVersion: '1.1.0-alpha',
+  appVersion: '1.1.0-beta',
   setAppName: (appName) => set({ appName }),
   setAppVersion: (appVersion) => set({ appVersion })
 }))
